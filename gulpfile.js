2e8cd608430b79adda4406eb9ae957ccd01834e1
Jakub Skokan <jakub.skokan@havefun.cz>
2016-01-09 16:47:44 +0100

    Add gulp task to generate html docs using jsdoc

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,28 @@
 var gulp = require('gulp'),
 	concat = require('gulp-concat'),
-	umd = require('gulp-umd');
+	umd = require('gulp-umd'),
+	jsdoc = require('gulp-jsdoc3');
+
+var src = [
+	'src/haveapi/client.js',
+	'src/haveapi/hooks.js',
+	'src/haveapi/http.js',
+	'src/haveapi/authentication.js',
+	'src/haveapi/authentication/base.js',
+	'src/haveapi/authentication/basic.js',
+	'src/haveapi/authentication/token.js',
+	'src/haveapi/base_resource.js',
+	'src/haveapi/resource.js',
+	'src/haveapi/action.js',
+	'src/haveapi/response.js',
+	'src/haveapi/resource_instance.js',
+	'src/haveapi/resource_instance_list.js',
+	'src/haveapi/exceptions.js',
+	'src/*.js',
+];
 
 gulp.task('scripts', function() {
-	return gulp.src([
-			'src/haveapi/client.js',
-			'src/haveapi/hooks.js',
-			'src/haveapi/http.js',
-			'src/haveapi/authentication.js',
-			'src/haveapi/authentication/base.js',
-			'src/haveapi/authentication/basic.js',
-			'src/haveapi/authentication/token.js',
-			'src/haveapi/base_resource.js',
-			'src/haveapi/resource.js',
-			'src/haveapi/action.js',
-			'src/haveapi/response.js',
-			'src/haveapi/resource_instance.js',
-			'src/haveapi/resource_instance_list.js',
-			'src/haveapi/exceptions.js',
-			'src/*.js',
-		])
+	return gulp.src(src)
 		.pipe(concat('haveapi-client.js'))
 		.pipe(umd({
 			exports: function (file) {
@@ -32,9 +35,15 @@ gulp.task('scripts', function() {
 		.pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('watch', function() {
-	gulp.watch('src/*.js', ['scripts']);
-	gulp.watch('src/**/*.js', ['scripts']);
+gulp.task('doc', function (cb) {
+	gulp.src(src.concat(['README.md']), {read: false}).pipe(jsdoc({
+		opts: { destination: "html_doc" }
+	}, cb));
 });
 
-gulp.task('default', ['scripts'])
+gulp.task('watch', function() {
+	gulp.watch('src/*.js', ['scripts', 'doc']);
+	gulp.watch('src/**/*.js', ['scripts', 'doc']);
+});
+
+gulp.task('default', ['scripts']);

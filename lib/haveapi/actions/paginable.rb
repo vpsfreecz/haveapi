456741aeca74b41c6c0680ab20adf6124ba0b76a
Jakub Skokan <jakub.skokan@havefun.cz>
2016-04-16 09:22:38 +0200

    Paginable: no default values for limit and offset

--- lib/haveapi/actions/paginable.rb
+++ lib/haveapi/actions/paginable.rb
@@ -2,10 +2,8 @@ module HaveAPI::Actions
   module Paginable
     def self.included(action)
       action.input do
-        integer :offset, label: 'Offset', desc: 'The offset of the first object',
-                default: 0
-        integer :limit, label: 'Limit', desc: 'The number of objects to retrieve',
-                default: 25
+        integer :offset, label: 'Offset', desc: 'The offset of the first object'
+        integer :limit, label: 'Limit', desc: 'The number of objects to retrieve'
       end
     end
   end

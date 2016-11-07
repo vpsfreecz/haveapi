0d09acf18e6c1289752ad94af7bd9c5b056b9dcb
Jakub Skokan <jakub.skokan@havefun.cz>
2016-11-07 08:35:32 +0100

    Add shortcut method HaveAPI::Client.new

--- lib/haveapi/client.rb
+++ lib/haveapi/client.rb
@@ -2,7 +2,12 @@ require 'require_all'
 require 'date'
 
 module HaveAPI
-  module Client ; end
+  module Client
+    # Shortcut to HaveAPI::Client::Client.new
+    def self.new(*args)
+      HaveAPI::Client::Client.new(*args)
+    end
+  end
 end
 
 require_rel 'client'

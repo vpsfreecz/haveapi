f0fc102f1386c818be46e4cf283141e55dc9eb36
Jakub Skokan <jakub.skokan@havefun.cz>
2016-11-13 16:47:45 +0100

    ClientExamples::JsClient: describe reply

--- lib/haveapi/client_examples/js_client.rb
+++ lib/haveapi/client_examples/js_client.rb
@@ -65,13 +65,84 @@ END
       out << '('
       out << "#{args.join(', ')}, " unless args.empty?
 
-      callback = <<END
-function (client, response) {
-  console.log('Response', response);
-}
-END
+      callback = "function (client, reply) {\n"
+      callback << "  console.log('Response', reply);\n"
+
+      if sample[:status]
+        callback << response(sample)
+
+      else
+        callback << error(sample)
+      end
+
+      callback << "}"
+
       out << callback.strip
       out << ');'
+      out
+    end
+
+    def response(sample)
+      out = ''
+
+      case action[:output][:layout]
+      when :hash
+        out << "# reply is an instance of HaveAPI.Client.Response\n"
+        out << "# reply.response() returns an object with output parameters:\n"
+        out << JSON.pretty_generate(sample[:response] || {}).split("\n").map do |v|
+          "  // #{v}"
+        end.join("\n")
+
+      when :hash_list
+        out << "# reply is an instance of HaveAPI.Client.Response\n"
+        out << "# reply.response() returns an array of objects:\n"
+        out << JSON.pretty_generate(sample[:response] || []).split("\n").map do |v|
+          "  // #{v}"
+        end.join("\n")
+
+      when :object
+        out << "  // reply is an instance of HaveAPI.Client.ResourceInstance\n"
+
+        (sample[:response] || {}).each do |k, v|
+          param = action[:output][:parameters][k]
+
+          if param[:type] == 'Resource'
+            out << "  // reply.#{k} = HaveAPI.Client.ResourceInstance("
+            out << "resource: #{param[:resource].join('.')}, "
+
+            if v.is_a?(::Hash)
+              out << v.map { |k,v| "#{k}: #{PP.pp(v, '').strip}" }.join(', ')
+            else
+              out << "id: #{v}"
+            end
+
+            out << ")\n"
+
+          elsif param[:type] == 'Custom' && (v.is_a?(::Hash) || v.is_a?(::Array))
+            json = JSON.pretty_generate(v).split("\n").map do |v|
+              "  // #{v}"
+            end.join("\n")
+
+            out << "  // reply.#{k} = #{json}"
+
+          else
+            out << "  // reply.#{k} = #{PP.pp(v, '')}"
+          end
+        end
+
+      when :object_list
+        out << "  // reply is an instance of HaveAPI.Client.ResourceInstanceList\n"
+      end
+
+      out
+
+    end
+
+    def error(sample)
+      out = ''
+      out << "  // reply.isOk() returns false\n"
+      out << "  // reply.message() returns the error message\n"
+      out << "  // reply.envelope.errors contains parameter errors\n"
       out
     end
   end

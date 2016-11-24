a54e8fb6219d9e0faba80f2479dfa336b3179317
Jakub Skokan <jakub.skokan@havefun.cz>
2016-11-24 09:55:11 +0100

    Version 0.7.0

--- dist/haveapi-client.js
+++ dist/haveapi-client.js
@@ -53,7 +53,7 @@ function Client(url, opts) {
 }
 
 /** @constant HaveAPI.Client.Version */
-Client.Version = '0.6.0';
+Client.Version = '0.7.0';
 
 /** @constant HaveAPI.Client.ProtocolVersion */
 Client.ProtocolVersion = '1.1';

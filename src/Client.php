b7c2244fd0bb81513f2508673d829f2ce17e0c02
Jakub Skokan <jakub.skokan@havefun.cz>
2017-04-22 14:19:26 +0200

    Version 0.9.0

--- src/Client.php
+++ src/Client.php
@@ -6,7 +6,7 @@ namespace HaveAPI;
  * A client for a HaveAPI based API.
  */
 class Client extends Client\Resource {
-	const VERSION = '0.8.0';
+	const VERSION = '0.9.0';
 	const PROTOCOL_VERSION = '1.2';
 
 	private $uri;

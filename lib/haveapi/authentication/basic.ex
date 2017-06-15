e017474d71e6acb87119d082f7aca05af037d39a
Jakub Skokan <jakub.skokan@havefun.cz>
2017-06-15 19:03:55 +0200

    Add HTTP basic auth helper

---
+++ lib/haveapi/authentication/basic.ex
@@ -0,0 +1,39 @@
+defmodule HaveAPI.Authentication.Basic do
+  @callback find_user(String.t, String.t) :: any
+
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour unquote(__MODULE__)
+
+      def name, do: :basic
+
+      def describe do
+        %{
+          description: "Authentication using HTTP basic. Username and password is passed " <>
+                       "via HTTP header. Its use is forbidden from web browsers."
+        }
+      end
+
+      def authenticate(conn) do
+        header = Enum.find(
+          conn.req_headers,
+          fn {k,v} -> k == "authorization" end
+        )
+
+        if header do
+          {_, auth} = header
+          [_, auth] = String.split(auth, ~r{\s}, trim: true)
+          [user, password] = auth |> Base.decode64! |> String.split(":")
+
+          find_user(user, password)
+
+        else
+          nil
+        end
+
+      rescue ArgumentError ->
+        nil
+      end
+    end
+  end
+end

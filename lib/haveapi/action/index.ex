09f35597b59d91bb94d50d6afacc24a472682250
Jakub Skokan <jakub.skokan@havefun.cz>
2017-06-22 19:09:56 +0200

    Action.Index: optionally return total item count

--- lib/haveapi/action/index.ex
+++ lib/haveapi/action/index.ex
@@ -12,4 +12,30 @@ defmodule HaveAPI.Action.Index do
   output :hash_list do
 
   end
+
+  meta :global do
+    input do
+      boolean :total_count
+    end
+
+    output do
+      integer :total_count
+    end
+  end
+
+  post_exec :add_total_count
+
+  def add_total_count(req, res) do
+    if req.meta && req.meta.total_count do
+      if res.meta do
+        put_in(res.meta[:total_count], req.context.action.count(req))
+
+      else
+        %{res | meta: %{total_count: req.context.action.count(req)}}
+      end
+
+    else
+      res
+    end
+  end
 end

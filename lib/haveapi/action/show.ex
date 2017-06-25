b2dd1ff95ae69271369e72ac80e54376a08e5bf4
Jakub Skokan <jakub.skokan@havefun.cz>
2017-06-25 19:19:05 +0200

    Action.Show.add_local_metadata: object id is alredy in req.params

--- lib/haveapi/action/show.ex
+++ lib/haveapi/action/show.ex
@@ -13,11 +13,10 @@ defmodule HaveAPI.Action.Show do
 
   post_exec :add_local_metadata
 
-  # TODO: mention/fix that we need output parameter `id` to be present
   def add_local_metadata(req, res) do
     if res.output[:id] do
       %{res | output: Map.put(res.output, :_meta, %{
-          url_params: (req.params |> Keyword.delete_first(:glob) |> Keyword.values) ++ [res.output.id],
+          url_params: (req.params |> Keyword.delete_first(:glob) |> Keyword.values),
           resolved: true
       })}
 

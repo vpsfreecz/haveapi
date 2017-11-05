f5ce0da19f71b0873c648b26cb52182cd6cc6c4d
Jakub Skokan <jakub.skokan@havefun.cz>
2017-11-05 14:02:52 +0100

    Ruby client: update RestClient to v2.0.2

--- clients/ruby/haveapi-client.gemspec
+++ clients/ruby/haveapi-client.gemspec
@@ -24,7 +24,7 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency 'activesupport', '>= 4.0'
   spec.add_runtime_dependency 'require_all'
-  spec.add_runtime_dependency 'rest-client', '~> 1.8.0'
+  spec.add_runtime_dependency 'rest-client', '~> 2.0.2'
   spec.add_runtime_dependency 'json'
   spec.add_runtime_dependency 'highline', '~> 1.7.8'
   spec.add_runtime_dependency 'ruby-progressbar', '~> 1.7.5'
--- clients/ruby/lib/restclient_ext/resource.rb
+++ clients/ruby/lib/restclient_ext/resource.rb
@@ -2,10 +2,11 @@ module RestClient
   class Resource
     def get_options(additional_headers={}, &block)
       headers = (options[:headers] || {}).merge(additional_headers)
-      Request.execute_options(options.merge(
-                          :method => :options,
-                          :url => url,
-                          :headers => headers), &(block || @block))
+      Request.execute(options.merge(
+        :method => :options,
+        :url => url,
+        :headers => headers
+      ), &(block || @block))
     end
   end
 end

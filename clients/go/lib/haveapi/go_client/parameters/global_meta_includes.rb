23b9cf405a79f06dbc118c7f22aeead2d26c69d4
Jakub Skokan <jakub.skokan@havefun.cz>
2019-04-27 10:25:57 +0200

    clients/go: add support for global meta parameter includes

---
+++ clients/go/lib/haveapi/go_client/parameters/global_meta_includes.rb
@@ -0,0 +1,17 @@
+require 'haveapi/go_client/parameters/base'
+
+module HaveAPI::GoClient
+  class Parameters::GlobalMetaIncludes < Parameters::Base
+    handle do |role, direction, name, desc|
+      role == :global_meta \
+        && direction == :input \
+        && name == 'includes' \
+        && desc[:type] == 'Custom'
+    end
+
+    protected
+    def do_resolve
+      @go_in_type = @go_out_type = 'string'
+    end
+  end
+end

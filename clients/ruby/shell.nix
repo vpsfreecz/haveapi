a4b75cf1d0b405292822243d86f19b42320b461d
Jakub Skokan <jakub.skokan@havefun.cz>
2019-04-29 12:39:17 +0200

    Add shell.nix files for ruby

---
+++ clients/ruby/shell.nix
@@ -0,0 +1,20 @@
+let
+  pkgs = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
+
+in stdenv.mkDerivation rec {
+  name = "haveapi-client";
+
+  buildInputs = with pkgs;[
+    ruby
+    git
+    openssl
+  ];
+
+  shellHook = ''
+    export GEM_HOME=$(pwd)/../../.gems
+    export PATH="$GEM_HOME/.gems/bin:$PATH"
+    gem install bundler
+    bundle install
+  '';
+}

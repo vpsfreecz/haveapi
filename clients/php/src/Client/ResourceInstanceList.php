dda033f4cfbe5fcb14b96c3d24a53469d9779f84
Jakub Skokan <jakub.skokan@havefun.cz>
2022-12-02 17:39:29 +0100

    clients/php: fix deprecation warnings on php 8.1

--- clients/php/src/Client/ResourceInstanceList.php
+++ clients/php/src/Client/ResourceInstanceList.php
@@ -11,7 +11,7 @@ class ResourceInstanceList implements \ArrayAccess, \Iterator {
 	private $items = array();
 	private $index = 0;
 	private $response;
-	
+
 	/**
 	 * @param Client $client
 	 * @param Action $action
@@ -19,40 +19,40 @@ class ResourceInstanceList implements \ArrayAccess, \Iterator {
 	 */
 	public function __construct($client, $action, $response) {
 		$this->response = $response;
-		
+
 		foreach($response->getResponse() as $item) {
 			$this->items[] = new ResourceInstance($client, $action, $item);
 		}
 	}
-	
+
 	/**
 	 * @return int object count
 	 */
 	public function count() {
 		return count($this->items);
 	}
-	
+
 	/**
 	 * @return Response
 	 */
 	public function getApiResponse() {
 		return $this->response;
 	}
-	
+
 	/**
 	 * @return ResourceInstance first object
 	 */
 	public function first() {
 		return $this->items[0];
 	}
-	
+
 	/**
 	 * @return ResourceInstance last object
 	 */
 	public function last() {
 		return $this->items[ $this->count() - 1 ];
 	}
-	
+
 	/**
 	 * Returns all objects in an array.
 	 * @return array
@@ -60,55 +60,55 @@ class ResourceInstanceList implements \ArrayAccess, \Iterator {
 	public function asArray() {
 		return $this->items;
 	}
-	
+
 	public function getMeta() {
 		return $this->response->getMeta();
 	}
-	
+
 	public function getTotalCount() {
 		$m = $this->getMeta();
-		
+
 		if (!$m || !isset($m->total_count))
 			return false;
-		
+
 		return $m->total_count;
 	}
-	
+
 	// ArrayAccess
-	public function offsetExists($offset) {
+	public function offsetExists($offset): bool {
 		return isSet($this->items[$offset]);
 	}
-	
-	public function offsetGet($offset) {
+
+	public function offsetGet($offset): mixed {
 		return $this->items[$offset];
 	}
-	
-	public function offsetSet($offset, $value) {
+
+	public function offsetSet($offset, $value): void {
 		$this->items[$offset] = $value;
 	}
-	
-	public function offsetUnset($offset) {
+
+	public function offsetUnset($offset): void {
 		unset($this->items[$offset]);
 	}
-	
+
 	// Iterator
-	public function current() {
+	public function current(): mixed {
 		return $this->items[$this->index];
 	}
-	
-	public function key() {
+
+	public function key(): mixed {
 		return $this->index;
 	}
-	
-	public function next() {
+
+	public function next(): void {
 		++$this->index;
 	}
-	
-	public function rewind() {
+
+	public function rewind(): void {
 		$this->index = 0;
 	}
-	
-	public function valid() {
+
+	public function valid(): bool {
 		return isSet($this->items[$this->index]);
 	}
 }

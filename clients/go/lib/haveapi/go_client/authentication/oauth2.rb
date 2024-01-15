9c066c91dfa00843effa889ea7cf147cbf588e24
Jakub Skokan <jakub.skokan@havefun.cz>
2024-01-15 18:30:41 +0100

    clients/go: add support for OAuth2 access tokens

---
+++ clients/go/lib/haveapi/go_client/authentication/oauth2.rb
@@ -0,0 +1,31 @@
+require 'haveapi/go_client/authentication/base'
+
+module HaveAPI::GoClient
+  class Authentication::OAuth2 < Authentication::Base
+    register :oauth2
+
+    # HTTP header the token is sent in
+    # @return [String]
+    attr_reader :http_header
+
+    # Token revocation URL
+    # @return [String]
+    attr_reader :revoke_url
+
+    def initialize(api_version, name, desc)
+      @http_header = desc[:http_header]
+      @revoke_url = desc[:revoke_url]
+    end
+
+    def generate(gen)
+      ErbTemplate.render_to_if_changed(
+        'authentication/oauth2.go',
+        {
+          package: gen.package,
+          auth: self,
+        },
+        File.join(gen.dst, 'auth_oauth2.go')
+      )
+    end
+  end
+end

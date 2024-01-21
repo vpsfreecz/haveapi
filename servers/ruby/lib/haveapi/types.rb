ef2b45bc85690e790163378b7fcb6fdf3a24d858
Jakub Skokan <jakub.skokan@havefun.cz>
2024-01-21 19:39:52 +0100

    servers/ruby: fix boolean input parameter conversion on ruby 3.2

--- servers/ruby/lib/haveapi/types.rb
+++ servers/ruby/lib/haveapi/types.rb
@@ -2,10 +2,12 @@
 module Boolean
   def self.to_b(str)
     return true if str === true
-    return true if str =~ /^(true|t|yes|y|1)$/i
-
     return false if str === false
-    return false if str =~ /^(false|f|no|n|0)$/i
+
+    if str.respond_to?(:=~)
+      return true if str =~ /^(true|t|yes|y|1)$/i
+      return false if str =~ /^(false|f|no|n|0)$/i
+    end
 
     false
   end
@@ -16,7 +18,7 @@ module Datetime
 end
 
 module Custom
-  
+
 end
 
 class Text < String

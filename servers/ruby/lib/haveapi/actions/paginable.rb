00f91023341248fed9b2d99232b3ceadff096264
Jakub Skokan <jakub.skokan@havefun.cz>
2024-06-23 09:32:38 +0200

    servers/ruby: add validator to offset/limit paginable parameters
    (cherry picked from commit a71fd5c4190b353cc59c7e5dee4e96d2204764c9)

--- servers/ruby/lib/haveapi/actions/paginable.rb
+++ servers/ruby/lib/haveapi/actions/paginable.rb
@@ -2,8 +2,10 @@ module HaveAPI::Actions
   module Paginable
     def self.included(action)
       action.input do
-        integer :offset, label: 'Offset', desc: 'The offset of the first object'
-        integer :limit, label: 'Limit', desc: 'The number of objects to retrieve'
+        integer :offset, label: 'Offset', desc: 'The offset of the first object',
+                         number: { min: 0 }
+        integer :limit, label: 'Limit', desc: 'The number of objects to retrieve',
+                        number: { min: 0 }
       end
     end
   end
